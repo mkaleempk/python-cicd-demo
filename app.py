0f2957d4e946024a77684de037a5080ba86e1568
Muhammad Kaleem <mkaleemsheikh@gmail.com>
2025-11-08 08:04:50 +0500

    jienkins freestyle

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
-# app.py
 from flask import Flask
-import socket
-import datetime
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return f"""
-    <html>
-    <head><title>Jenkins Python CI/CD Demo</title></head>
-    <body style='text-align:center;font-family:sans-serif;'>
-        <h1 style='color:green;'>🚀 Python Web App via Jenkins CI/CD</h1>
-        <p>✅ Deployment successful!</p>
-        <p>🕒 Time now: {datetime.datetime.now()}</p>
-        <p>💻 Hostname: {socket.gethostname()}</p>
-    </body>
-    </html>
-    """
+    return "Hello from Flask on EC2 via Jenkins!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    # Important: host='0.0.0.0' makes it public
+    app.run(host='0.0.0.0', port=5000)
